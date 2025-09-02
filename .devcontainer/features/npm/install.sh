0880fb1804791a75504a777fe8b2edb407001839
k5-mot <34744243+k5-mot@users.noreply.github.com>
2025-09-02 23:55:36 +0000

    A

--- .devcontainer/features/npm/install.sh
+++ .devcontainer/features/npm/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-IFS="," read -r -a DEFAULT_UTILS <<< "${TOOLSTOINSTALL:-git-cz,npm-check-updates,@anthropic-ai/claude-code,@openai/codex}"
+IFS="," read -r -a DEFAULT_UTILS <<< "${TOOLSTOINSTALL:-npm,yarn,pnpm,git-cz,npm-check-updates}"
 set -e
 
 for tool in "${DEFAULT_UTILS[@]}"; do
